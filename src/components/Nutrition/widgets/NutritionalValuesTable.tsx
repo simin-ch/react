a4f8ee2f4abffea9d910ffb293e85310d0b1d9dc
Simin Cheng <sc1040@students.waikato.ac.nz>
2025-08-27 18:05:11 +1200

    Modified the NutritionalValuesTable - Add an optional 'showPrecisionToggle' property - Using useState hook to manage precision state - update numberGramLocale calls to include the precision parameter - Add toggle functionality for switching between integer and decimal display

--- src/components/Nutrition/widgets/NutritionalValuesTable.tsx
+++ src/components/Nutrition/widgets/NutritionalValuesTable.tsx
@@ -1,103 +1,121 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Box, IconButton, Tooltip } from "@mui/material";
+import { ToggleOff, ToggleOn } from "@mui/icons-material";
 import { NutritionalValues } from "components/Nutrition/helpers/nutritionalValues";
 import { useTranslation } from "react-i18next";
 import { numberGramLocale, numberLocale, numberPercentLocale } from "utils/numbers";
+import { useState } from "react";
 
-export const NutritionalValuesTable = (props: { values: NutritionalValues }) => {
+export const NutritionalValuesTable = (props: { values: NutritionalValues, showPrecisionToggle?: boolean }) => {
     const [t, i18n] = useTranslation();
+    const [precision, setPrecision] = useState(0);
 
-    return <TableContainer>
-        <Table size="small">
-            <TableHead>
-                <TableRow>
-                    <TableCell>{t('nutrition.macronutrient')}</TableCell>
-                    <TableCell align="right">{t('total')}</TableCell>
-                    <TableCell align="right">{t('nutrition.percentEnergy')}</TableCell>
-                    {/*<TableCell align="right">{t('nutrition.gPerBodyKg')}</TableCell>*/}
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                <TableRow>
-                    <TableCell>{t('nutrition.energy')}</TableCell>
-                    <TableCell align="right">
-                        {t('nutrition.valueEnergyKcalKj', {
-                            kcal: numberLocale(props.values.energy, i18n.language),
-                            kj: numberLocale(props.values.energyKj, i18n.language)
-                        })}
-                    </TableCell>
-                    <TableCell align="right"></TableCell>
-                    {/*<TableCell align="right"></TableCell>*/}
-                </TableRow>
-                <TableRow>
-                    <TableCell>{t('nutrition.protein')}</TableCell>
-                    <TableCell align="right">
-                        {numberGramLocale(props.values.protein, i18n.language)}
-                    </TableCell>
-                    <TableCell align="right">
-                        {numberPercentLocale(props.values.percent.protein, i18n.language)}
-                    </TableCell>
-                    {/*<TableCell align="right">...</TableCell>*/}
-                </TableRow>
-                <TableRow>
-                    <TableCell>{t('nutrition.carbohydrates')}</TableCell>
-                    <TableCell align="right">
-                        {numberGramLocale(props.values.carbohydrates, i18n.language)}
-                    </TableCell>
-                    <TableCell align="right">
-                        {numberPercentLocale(props.values.percent.carbohydrates, i18n.language)}
-                    </TableCell>
-                    {/*<TableCell align="right">...</TableCell>*/}
-                </TableRow>
-                <TableRow>
-                    <TableCell sx={{ pl: 5 }}>{t('nutrition.ofWhichSugars')}</TableCell>
-                    <TableCell align="right">
-                        {numberGramLocale(props.values.carbohydratesSugar, i18n.language)}
-                    </TableCell>
-                    <TableCell align="right"></TableCell>
-                    {/*<TableCell align="right"></TableCell>*/}
-                </TableRow>
-                <TableRow>
-                    <TableCell>{t('nutrition.fat')}</TableCell>
-                    <TableCell align="right">
-                        {numberGramLocale(props.values.fat, i18n.language)}
-                    </TableCell>
-                    <TableCell align="right">
-                        {numberPercentLocale(props.values.percent.fat, i18n.language)}
-                    </TableCell>
-                    {/*<TableCell align="right">...</TableCell>*/}
-                </TableRow>
-                <TableRow>
-                    <TableCell sx={{ pl: 5 }}>{t('nutrition.ofWhichSaturated')}</TableCell>
-                    <TableCell align="right">
-                        {numberGramLocale(props.values.fatSaturated, i18n.language)}
-                    </TableCell>
-                    <TableCell align="right"></TableCell>
-                    {/*<TableCell align="right"></TableCell>*/}
-                </TableRow>
+    const togglePrecision = () => {
+        setPrecision(precision === 0 ? 1 : 0);
+    };
+    
+    return <Box>
+        {props.showPrecisionToggle && (
+            <Box display="flex" justifyContent="flex-end" mb={1}>
+                <Tooltip title={precision === 0 ? "Show decimal places" : "Show whole numbers"}>
+                    <IconButton onClick={togglePrecision} size="small">
+                        {precision === 0 ? <ToggleOff /> : <ToggleOn />}
+                    </IconButton>
+                </Tooltip>
+            </Box>
+        )}
+        <TableContainer>
+            <Table size="small">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>{t('nutrition.macronutrient')}</TableCell>
+                        <TableCell align="right">{t('total')}</TableCell>
+                        <TableCell align="right">{t('nutrition.percentEnergy')}</TableCell>
+                        {/*<TableCell align="right">{t('nutrition.gPerBodyKg')}</TableCell>*/}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    <TableRow>
+                        <TableCell>{t('nutrition.energy')}</TableCell>
+                        <TableCell align="right">
+                            {t('nutrition.valueEnergyKcalKj', {
+                                kcal: numberLocale(props.values.energy, i18n.language),
+                                kj: numberLocale(props.values.energyKj, i18n.language)
+                            })}
+                        </TableCell>
+                        <TableCell align="right"></TableCell>
+                        {/*<TableCell align="right"></TableCell>*/}
+                    </TableRow>
+                    <TableRow>
+                        <TableCell>{t('nutrition.protein')}</TableCell>
+                        <TableCell align="right">
+                            {numberGramLocale(props.values.protein, i18n.language, precision)}
+                        </TableCell>
+                        <TableCell align="right">
+                            {numberPercentLocale(props.values.percent.protein, i18n.language)}
+                        </TableCell>
+                        {/*<TableCell align="right">...</TableCell>*/}
+                    </TableRow>
+                    <TableRow>
+                        <TableCell>{t('nutrition.carbohydrates')}</TableCell>
+                        <TableCell align="right">
+                            {numberGramLocale(props.values.carbohydrates, i18n.language, precision)}
+                        </TableCell>
+                        <TableCell align="right">
+                            {numberPercentLocale(props.values.percent.carbohydrates, i18n.language)}
+                        </TableCell>
+                        {/*<TableCell align="right">...</TableCell>*/}
+                    </TableRow>
+                    <TableRow>
+                        <TableCell sx={{ pl: 5 }}>{t('nutrition.ofWhichSugars')}</TableCell>
+                        <TableCell align="right">
+                            {numberGramLocale(props.values.carbohydratesSugar, i18n.language, precision)}
+                        </TableCell>
+                        <TableCell align="right"></TableCell>
+                        {/*<TableCell align="right"></TableCell>*/}
+                    </TableRow>
+                    <TableRow>
+                        <TableCell>{t('nutrition.fat')}</TableCell>
+                        <TableCell align="right">
+                            {numberGramLocale(props.values.fat, i18n.language, precision)}
+                        </TableCell>
+                        <TableCell align="right">
+                            {numberPercentLocale(props.values.percent.fat, i18n.language)}
+                        </TableCell>
+                        {/*<TableCell align="right">...</TableCell>*/}
+                    </TableRow>
+                    <TableRow>
+                        <TableCell sx={{ pl: 5 }}>{t('nutrition.ofWhichSaturated')}</TableCell>
+                        <TableCell align="right">
+                            {numberGramLocale(props.values.fatSaturated, i18n.language, precision)}
+                        </TableCell>
+                        <TableCell align="right"></TableCell>
+                        {/*<TableCell align="right"></TableCell>*/}
+                    </TableRow>
 
-                <TableRow>
-                    <TableCell>{t('nutrition.others')}</TableCell>
-                    <TableCell> </TableCell>
-                    <TableCell align="right"></TableCell>
-                    {/*<TableCell align="right"></TableCell>*/}
-                </TableRow>
-                <TableRow>
-                    <TableCell>{t('nutrition.fibres')}</TableCell>
-                    <TableCell align="right">
-                        {numberGramLocale(props.values.fiber, i18n.language)}
-                    </TableCell>
-                    <TableCell align="right"></TableCell>
-                    {/*<TableCell align="right"></TableCell>*/}
-                </TableRow>
-                <TableRow>
-                    <TableCell>{t('nutrition.sodium')}</TableCell>
-                    <TableCell align="right">
-                        {numberGramLocale(props.values.sodium, i18n.language)}
-                    </TableCell>
-                    <TableCell align="right"></TableCell>
-                    {/*<TableCell align="right"></TableCell>*/}
-                </TableRow>
-            </TableBody>
-        </Table>
-    </TableContainer>;
+                    <TableRow>
+                        <TableCell>{t('nutrition.others')}</TableCell>
+                        <TableCell> </TableCell>
+                        <TableCell align="right"></TableCell>
+                        {/*<TableCell align="right"></TableCell>*/}
+                    </TableRow>
+                    <TableRow>
+                        <TableCell>{t('nutrition.fibres')}</TableCell>
+                        <TableCell align="right">
+                            {numberGramLocale(props.values.fiber, i18n.language, precision)}
+                        </TableCell>
+                        <TableCell align="right"></TableCell>
+                        {/*<TableCell align="right"></TableCell>*/}
+                    </TableRow>
+                    <TableRow>
+                        <TableCell>{t('nutrition.sodium')}</TableCell>
+                        <TableCell align="right">
+                            {numberGramLocale(props.values.sodium, i18n.language, precision)}
+                        </TableCell>
+                        <TableCell align="right"></TableCell>
+                        {/*<TableCell align="right"></TableCell>*/}
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </Box>;
 };
