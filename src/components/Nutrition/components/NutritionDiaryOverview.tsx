5517b7dffa2efb4c59b11eff30115c141607dfff
Simin Cheng <sc1040@students.waikato.ac.nz>
2025-09-20 15:20:22 +1200

    Added toggle button to page header options menu

--- src/components/Nutrition/components/NutritionDiaryOverview.tsx
+++ src/components/Nutrition/components/NutritionDiaryOverview.tsx
@@ -1,11 +1,10 @@
-import { Stack, Typography, Box, IconButton, Tooltip } from "@mui/material";
-import { ToggleOff, ToggleOn } from "@mui/icons-material";
+import { Stack, Typography } from "@mui/material";
 import { LoadingPlaceholder } from "components/Core/LoadingWidget/LoadingWidget";
 import { WgerContainerRightSidebar } from "components/Core/Widgets/Container";
 import { useFetchNutritionalPlanDateQuery } from "components/Nutrition/queries";
 import { IngredientDetailTable } from "components/Nutrition/widgets/IngredientDetailTable";
 import { LoggedPlannedNutritionalValuesTable } from "components/Nutrition/widgets/LoggedPlannedNutritionalValuesTable";
-import { useNutritionPrecision } from "../context/NutritionPrecisionContext";
+import { PrecisionToggleButton } from "components/Nutrition/widgets/PrecisionToggleButton";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
@@ -23,31 +22,17 @@ export const NutritionDiaryOverview = () => {
     const date = new Date(params.date!);
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const planQuery = useFetchNutritionalPlanDateQuery(planId, params.date!);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { precision, togglePrecision } = useNutritionPrecision();
 
     return planQuery.isLoading
         ? <LoadingPlaceholder />
         : <WgerContainerRightSidebar
             title={t('nutrition.nutritionalDiary')}
+            optionsMenu={<PrecisionToggleButton />}
             mainContent={<>
                 <Stack spacing={2}>
-                    {/* Date with precision toggle */}
-                    <Box display="flex" justifyContent="space-between" alignItems="center" mb={1}>
-                        <Typography gutterBottom variant="h4">
-                            {date.toLocaleDateString()}
-                        </Typography>
-                               <Box display="flex" alignItems="center" gap={1}>
-                                   <Typography variant="body2" color="text.secondary">
-                                       {precision === 0 ? "Whole numbers" : "One decimal"}
-                                   </Typography>
-                                   <Tooltip title={precision === 0 ? "Show one decimal places" : "Show whole numbers"}>
-                                       <IconButton onClick={togglePrecision} size="small">
-                                           {precision === 0 ? <ToggleOff /> : <ToggleOn />}
-                                       </IconButton>
-                                   </Tooltip>
-                               </Box>
-                    </Box>
+                    <Typography gutterBottom variant="h4">
+                        {date.toLocaleDateString()}
+                    </Typography>
                     
                     <LoggedPlannedNutritionalValuesTable
                         logged={planQuery.data!.loggedNutritionalValuesDate(date)}
